3001eb7cfa148d62affacdb3038ecbca48f9f23e
G <59087860+p83m@users.noreply.github.com>
2025-11-03 16:31:57 +0100

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />
+  </React.StrictMode>
+);
