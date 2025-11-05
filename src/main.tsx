ad1bc82a05f1dda0df1cc56c164f7487a8dbbcf2
G <59087860+p83m@users.noreply.github.com>
2025-11-05 10:10:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
